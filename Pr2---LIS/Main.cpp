6d2045e38d6eb52f703f3c4f622171e1a25ed6fd
rikachka <rika.95@mail.ru>
2014-03-22 18:43:19 +0300

    Update Main.cpp

--- Pr2---LIS/Main.cpp
+++ Pr2---LIS/Main.cpp
@@ -1,35 +1,9 @@
 #include "stdafx.h"
 #include <vector>
 #include <iostream>
+#include <algorithm>
 #include "conio.h"
 
-
-
-int binsearch(int element, const std::vector<int>& vector, size_t begin, size_t end) // [begin, end]
-{
-	size_t median = (begin + end) / 2;
-	if (vector[median] > element)
-	{
-		if (median == begin) return begin;
-		else return binsearch(element, vector, begin, median);
-	}
-	else if (vector[median] < element)
-	{
-		if (median + 1 >= vector.size() || vector[median + 1] > element)
-			return median + 1;
-		else return binsearch(element, vector, median + 1, end);
-	}
-	else if (vector[median] == element)
-	{
-		return median;
-	}
-}
-
-int binsearch(int element, const std::vector<int>& vector)
-{
-	if (vector.size() == 0) return -1;
-	else return binsearch(element, vector, 0, vector.size() - 1);
-}
 
 
 void lis(const std::vector<int>& vector)
@@ -44,34 +18,30 @@ void lis(const std::vector<int>& vector)
 	for (size_t index = 0; index < vector.size(); index++)
 	{
 		int element = vector[index];
-		int element_place = binsearch(element, vector_value);
+		int element_place = std::lower_bound(vector_value.begin(), vector_value.end(), element) - vector_value.begin();
 		// обновляем первый, который не меньше
 		if (element_place < vector_value.size())
 		{
 			vector_value[element_place] = element;
 			vector_index[element_place] = index;
-			if (element_place > 0)
-				previous_index.push_back(vector_index[element_place - 1]);
-			else previous_index.push_back(-1);
 		}
 		else 
 		{
 			vector_value.push_back(element);
 			vector_index.push_back(index);
-			if (element_place > 0)
-				previous_index.push_back(vector_index[element_place - 1]);
-			else previous_index.push_back(-1);
 		}
+		if (element_place > 0)
+			previous_index.push_back(vector_index[element_place - 1]);
+		else previous_index.push_back(-1);
 	}
 
 	std::vector<int> result;
 	int index = vector_index[vector_index.size() - 1];
-	do
+	while (previous_index[index] != -1)
 	{
 		result.push_back(vector[index]);
 		index = previous_index[index];
 	}
-	while (previous_index[index] != -1);
 	result.push_back(vector[index]);
 
 	for (size_t i = 1; i <= result.size(); i++)
@@ -79,10 +49,8 @@ void lis(const std::vector<int>& vector)
 }
 
 
-
-int main()
+void CreateYourVector(std::vector<int>& vector)
 {
-	std::vector<int> vector;
 	size_t vector_size;
 	std::cin >> vector_size;
 	for (size_t i = 0; i < vector_size; i++)
@@ -91,6 +59,25 @@ int main()
 		std::cin >> element;
 		vector.push_back(element);
 	}
+}
+
+// http://informatics.mccme.ru/mod/statements/view3.php?id=766&chapterid=1794#1
+void CreateTestVector(std::vector<int>& vector)
+{
+	size_t vector_size, a, k, b, m;
+	std::cin >> vector_size >> a >> k >> b >> m;
+	for (size_t i = 0; i < vector_size; i++)
+	{
+		vector.push_back(a);
+		a = (k*a + b) % m;
+	}
+}
+
+
+int main()
+{
+	std::vector<int> vector;
+	CreateTestVector(vector);
 
 	lis(vector);
 
